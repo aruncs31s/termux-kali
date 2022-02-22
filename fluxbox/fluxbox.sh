96149e51534e550d317419ee2dd2898e30a06492
aruncs31s <aruncs31ss@gmail.com>
2022-02-23 00:17:40 +0530

    update

--- fluxbox/fluxbox.sh
+++ fluxbox/fluxbox.sh
@@ -2,4 +2,11 @@
 sudo apt update 
 sudo apt install fluxbox tightvncserver -y
 mkdir ~/.vnc &>/dev/null
-mkdir ~/.vnc
+wget https://raw.githubusercontent.com/aruncs31s/termux-kali/main/fluxbox/vncstart -P /usr/local/bin
+wget https://raw.githubusercontent.com/aruncs31s/termux-kali/main/fluxbox/xstartup -P ~/.vnc/
+wget https://raw.githubusercontent.com/aruncs31s/termux-kali/main/fluxbox/vncstop -P /usr/local/bin
+
+chmod +x /usr/local/bin/vncstart
+chmod +x /usr/local/bin/vncstop
+
+printf "\nFluxbox wm is now installed  is now installed\n\nYou can start the vnc by typing vncstart \n vncstop to stop the vnc server\n"
